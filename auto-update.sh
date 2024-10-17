cea6119797cc5e3e428c77bcd2b7488940719b06
sunrisepeak <speakshen@163.com>
2024-10-17 14:35:34 +0800

    update script

--- auto-update.sh
+++ auto-update.sh
@@ -1,2 +1,21 @@
+#!/bin/bash
+# - security-only-ready, dont modify this file
 #npm init vuepress-theme-hope@latest home
 #npm run docs:dev
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+current_time=$(date "+%Y-%m-%d %H:%M:%S")
+
+echo "----------------------------------------------------"
+echo "--------- D2Learn - $current_time ---------"
+echo "----------------------------------------------------"
+
+echo "--------- Sync Courses ---------"
+cd $SCRIPT_DIR/courses
+bash auto-update.sh
+
+echo "--------- Sync Home ---------"
+git pull origin main
+cd $SCRIPT_DIR/home
+npm run docs:build
